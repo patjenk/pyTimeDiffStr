1a5bd9796119fcda141379c0177917380be36d85
Patrick Jenkins <me@patjenk.com>
2021-08-06 17:04:25 +0000

    Correcting mistakes in setup.py so that this package installs with pip.

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import setup
 setup(
     name="pyTimeDiffStr",
     version="0.0.1",
-    description="A package that provides a str description of the difference between two dates.",
-    url="git@github.com:patjenk/pyTimeDiffStr.git",
     author="Patrick Jenkins",
     author_email="me@patjenk.com",
+    description="A package that provides a str description of the difference between two dates.",
+    url="https://github.com/patjenk/pyTimeDiffStr",
     license="",
-    packages=[],
-    zip_save=False
+    packages=['pyTimeDiffStr'],
+    classifiers=[
+        "Programing Language :: Python :: 3",
+    ],
 )
 
