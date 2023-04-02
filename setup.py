ad1605bc9d84db92ed19dcea65ef2b94766c4851
Patrick Jenkins <me@patjenk.com>
2023-04-01 21:06:36 -0400

    Bumping version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pyTimeDiffStr",
-    version="0.0.1",
+    version="0.0.2",
     author="Patrick Jenkins",
     author_email="me@patjenk.com",
     description="A package that provides a str description of the difference between two dates.",
